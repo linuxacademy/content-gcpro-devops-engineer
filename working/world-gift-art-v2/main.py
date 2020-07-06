43f8d48f0de1d896be669e700b8bc1e127a3cc26
Matthew Ulasien <mulasien@gmail.com>
2020-07-06 15:45:28 -0500

    replaced old v2 with v3 materials

--- working/world-gift-art-v2/main.py
+++ working/world-gift-art-v2/main.py
@@ -21,5 +21,5 @@ app = worldgiftart.create_app(config)
 
 # This is only used when running locally. When running live, gunicorn runs
 # the application.
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug=True)
