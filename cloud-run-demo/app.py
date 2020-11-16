a4c3e1ef99b1bd44fe8343b6d2134edaa66f3aff
Matthew Ulasien <mulasien@gmail.com>
2020-11-16 14:32:06 -0600

    added cloud run demo folder/files

---
+++ cloud-run-demo/app.py
@@ -0,0 +1,14 @@
+import os
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_engineers():
+    return render_template("webpage.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
